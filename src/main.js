fe3bd3e24de63a3f18a713c77e2a3d22b4dd2551
Linus Jansson <00jansson@gmail.com>
2020-01-16 08:54:39 +0100

    asd

--- src/main.js
+++ src/main.js
@@ -1,10 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 
 Vue.config.productionTip = false
+Vue.config.performance = true;
 
-new Vue({
+import Nav from './components/NavBar.vue';
+
+Vue.component('nav-bar', Nav);
+
+const requireComponent = require.context('./components', true, /[A-Z]\w+\.(vue|js)$/)
+
+requireComponent.keys().forEach(fileName => {
+  // Get component config
+  const componentConfig = requireComponent(fileName)
+
+  // Get PascalCase name of component
+  const componentName = upperFirst(
+    camelCase(
+      // Gets the file name regardless of folder depth
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+
+
+  // Register component globally
+  Vue.component(
+    componentName,
+
+    componentConfig.default || componentConfig
+  )
+})
+
+
+window.app = new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+window.vm = {};
--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/maze',
+    name: 'Maze',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Maze.vue')
   }
 ]
 
